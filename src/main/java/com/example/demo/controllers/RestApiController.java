abb3a2c7e4cbb942d081cb10e4baa8176a6545cd
Hardik <hardiksavsani2828@gmail.com>
2024-11-30 17:18:06 +0200

    feat: add answer history endpoints

--- src/main/java/com/example/demo/controllers/RestApiController.java
+++ src/main/java/com/example/demo/controllers/RestApiController.java
@@ -2,10 +2,10 @@ package com.example.demo.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
+// import io.swagger.v3.oas.annotations.media.Content;
+// import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+// import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.HashMap;
